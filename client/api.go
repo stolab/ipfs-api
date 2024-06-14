73ab445a42221f192cf8d756c14ad3e9a55ed72a
arnaud <a.stoz@cylab.be>
2024-06-14 10:45:33 +0200

    rewrite documentation to follow the godoc standard

--- client/api.go
+++ client/api.go
@@ -1,3 +1,8 @@
+// Package client implement a client to the IPFS api
+//
+// This client is written based on the kubo rpc api https://docs.ipfs.tech/reference/kubo/rpc/ 
+// This package can be used with a local instance of kubo running or with publicly accessible endpoint
+// All contribution/suggestions are welcome.
 package client
 
 import (
@@ -13,6 +18,7 @@ import (
 	"time"
 )
 
+// The current path to the kubo api as mentionned in : https://docs.ipfs.tech/reference/kubo/rpc/
 const apiPath = "/api/v0/"
 
 var (
@@ -22,20 +28,16 @@ var (
 	}
 )
 
+// A client represent the connection to the RPC API endpoint
 type Client struct {
 	base *url.URL
 	httpClient *http.Client
 	url string
 }
 
-/*
-* Used to initialize a new client
-* @param:
-*		- url: the URL where the endpoint is located
-*		- timeout: the value of the timeout in seconde
-* @return:
-*		- *Client: a pointer to a client structure or nil if an error occured
-*/
+// NewIPFSApi return a Client struct based on the parameter given.
+// The parameters are the URL of the endpoint
+// and a timeout for the connection (default should be 4)
 func NewIPFSApi(Url string, timeout int) (*Client, error) {
 	parsedUrl, err := url.Parse(Url)
 	if err != nil {
@@ -53,24 +55,17 @@ func NewIPFSApi(Url string, timeout int) (*Client, error) {
 	}, nil
 }
 
-/*
-* Wrapper when a local api node is running
-*/
+// Wrapper to NewIPFSApi to use when a local node is running
+// on localhost port 5001
 func NewLocalApi() (*Client, error) {
     return NewIPFSApi("http://127.0.0.1:5001", 4)
 }
 
-/*
-* this function is used to add a file to IPFS 
-* (ie: upload a file)
-* it can only act on a Client structure
-* @params:
-*			- pathName: the pathName to add to IPFS (can be an entire directory)
-* @return:
-*           - an IPFSResponse struct
-* 			- an error if it occured and nil otherwise
-*/
-//NOTE By default the file will be pinned. Can pass the argument pin:false if we don't want to
+// The add function upload a new file to IPFS
+// It takes the path to the file to upload as a parameter
+// Upon successful upload it return an IPFSResponse struct and nil
+// In case of error the IPFSResponse is set to nil and an error is returned
+//NOTE By default the file will be pinned.
 func (client *Client) Add(pathName string) (*IPFSResponse, error) {
 	// initalizing variable needed
 	var apiResponse *http.Response
@@ -84,7 +79,7 @@ func (client *Client) Add(pathName string) (*IPFSResponse, error) {
 	}
 	writer.Close()
 
-	// The actual sending part
+	// The sending part
 	req, err := http.NewRequest("POST", client.url + apiEndpoint["add"] , multiPartBody)
 	contentType := fmt.Sprintf("multipart/form-data; boundary=%s", boundary)
 	req.Header.Set("Content-Type", contentType)
@@ -101,18 +96,13 @@ func (client *Client) Add(pathName string) (*IPFSResponse, error) {
 
 }
 
-/*
-* function used to facilitate the creation of the multipart body 
-* it first check if the pathname provide is a directory.
-* if its a file it create the multipart body with the
-* content of the file.
-* @parms:
-*		- pathname: the pathname to create the multipart for
-*		- writer: the multipart.writer used to create it.
-* @return:
-*		- a string representing the boundary of the multipart body
-*		- error if any., nil otherwise
-*/
+// Internal function to facilitate the creation of the multipart body 
+// it first check if the pathname provide is a directory.
+// if its a file it create the multipart body with the
+// content of the file.
+// it takes two argument, the pathname and the writer to write to
+// Upon success it return the string representing the boundary of the multipart body
+// If a failure occur return nil and the error
 func createMultiPartBody(pathName string, writer *multipart.Writer) (string, error){
 	// intializing variable
 	var err error
@@ -146,10 +136,8 @@ func createMultiPartBody(pathName string, writer *multipart.Writer) (string, err
 	return writerBoundary, nil
 }
 
-/*
-* Used when the Pathname is a directory
-* it loops on the all the file in the directory and create a big multipart body 
-*/
+// Used when the Pathname is a directory
+// it loops on the all the file in the directory and create a big multipart body 
 func createDirectoryMultiPartBody(pathName string, multiPartBody *multipart.Writer) (string, error){
 	// read the dir
 	var writerBoundary string
@@ -172,11 +160,10 @@ func createDirectoryMultiPartBody(pathName string, multiPartBody *multipart.Writ
 	return writerBoundary, nil
 }
 
-/*
-* Cat api call
-* Giving a CID will retrieve the content of it in the body of
-* request.response.
-*/
+// Cat function retrieve the content of file stored in IPFS based on its CID
+// It takes the CID of the object to retrieve as input
+// Return the HTTP.Response upon successful execution
+// Return nil and the error if an error occured
 func (client *Client) Cat(id string) (*http.Response, error) {
 	//initialize variable
 	var apiResponse *http.Response
@@ -193,12 +180,16 @@ func (client *Client) Cat(id string) (*http.Response, error) {
 	return apiResponse, nil
 }
 
+// IPFSResponse represent the response received from an IPFS node
+// upon successful upload of a file
 type IPFSResponse struct {
-    Name  string `json:"Name"`
-    Hash string `json:"Hash"`
-    Size string `json:"Size"`
+    Name  string `json:"Name"` // the name of the uploaded file
+    Hash string `json:"Hash"` // the CID of the uploaded file
+    Size string `json:"Size"` // the size of the uploaded file
 }
 
+// Internal function to translate and http.Response received from an IPFS API endpoint
+// to an IPFSResponse struct
 func readIPFSResponse(resp *http.Response) *IPFSResponse {
     ret := new(IPFSResponse)
     defer resp.Body.Close()
